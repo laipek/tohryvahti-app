bff5a3ffd5caf45faabf561f4bf89b43de219ba4
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2025-09-22 16:03:21 +0300

    Clean vite.config.ts and add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+const isProd = process.env.NODE_ENV === "production";
+const isReplit = process.env.REPL_ID !== undefined;
+
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+    plugins: [
+        react(),
+        runtimeErrorOverlay(),
+        ...( !isProd && isReplit
+            ? [
+                await import("@replit/vite-plugin-cartographer").then((m) => m.cartographer()),
+            ]
+            : []),
+    ],
+    root: path.resolve(import.meta.dirname, "client"),
+
+    publicDir: path.resolve(import.meta.dirname, "public"),
+
+    build: {
+        outDir: path.resolve(import.meta.dirname, "dist"),
+        emptyOutDir: true,
     },
-  },
-  root: path.resolve(import.meta.dirname, "client"),
-  build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
-  },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
+
+    resolve: {
+        alias: {
+            "@": path.resolve(import.meta.dirname, "client", "src"),
+            "@shared": path.resolve(import.meta.dirname, "shared"),
+            "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+        },
     },
-  },
+
+    server: {
+        proxy: {
+            "/api": "http://localhost:5000",
+        },
+        fs: {
+            strict: true,
+            deny: ["**/.*"],
+        },
+    },
 });
